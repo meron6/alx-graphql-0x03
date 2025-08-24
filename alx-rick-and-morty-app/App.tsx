b5a2fa624718f96c6baccb73a39be7a9b985fd4a
meron6 <khozameron5@gmail.com>
2025-08-24 20:24:03 +0000

    alx-graphql-0x03

---
+++ alx-rick-and-morty-app/App.tsx
@@ -0,0 +1,10 @@
+import ErrorBoundary from './components/ErrorBoundary';
+
+function App() {
+  return (
+    <ErrorBoundary>
+      {/* Your existing app components */}
+    </ErrorBoundary>
+  );
+}
+
